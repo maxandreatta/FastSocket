13dbc12ae9e79b5f5547234f1832b83c24b34fe0
Vinzenz Weist <vinzenz.weist@telekom.de>
2019-04-04 20:24:21 +0200

    [B] gitignore DS_Store Shit

--- FastSocketTests/FastSocketTests.swift
+++ FastSocketTests/FastSocketTests.swift
@@ -21,25 +21,32 @@ class FastSocketTests: XCTestCase {
 
     func testExample() {
         let exp = expectation(description: "Blub")
-        let buffer = Data(count: 2000000)
+        let buffer = Data(count: 100000)
         var core = Array<FastSocket>()
         var datacount = 0
         for i in 0...9 {
             core.append(FastSocket(host: "socket.weist.it", port: 3333))
             core[i].on.ready = {
-                core[i].send(data: buffer)
+                //core[i].send(data: buffer)
+                core[i].send(text: "1000000")
             }
             core[i].on.text = { text in
                 print("RECEIVED FROM: \(i) THIS COUNT: \(text)")
+                //core[i].send(data: buffer)
             }
             core[i].on.binary = { data in
                 print("RECEIVED FROM: \(i) THIS COUNT: \(data.count)")
-                //core.send(data: buffer)
+                core[i].send(text: "1000000")
             }
-            core[i].on.writtenData = { count in
+//            core[i].on.writtenData = { count in
+//                datacount += count
+//                print(datacount)
+//            }
+            core[i].on.receivedData = { count in
                 datacount += count
                 print(datacount)
             }
+
             core[i].connect()
         }
         wait(for: [exp], timeout: 100.0)
